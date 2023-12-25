362b59131946afd49c808599810b7f6a87378e93
ajayawachar400@gmail.com <ajay.awachar@udchalo.com>
2023-12-25 18:23:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Got Get Successfully', app: 'express'});
+})
+app.post('/', (req, res) => {
+    res.send('Got Post request Successfully');
+})
+
+const port = process.env.PORT || 9876;
+app.listen(port, () => {
+    console.log(`server listning on port ${port}`);
+});
